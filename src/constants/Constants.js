62dd0a9845b6c15416948f26b522919337eda2f8
Andrew Alexander <andy@Andrews-MacBook-Pro-2.local>
2019-09-08 01:36:50 -0400

    Cleanup 11

--- src/constants/Constants.js
+++
@@ -1,9 +0,0 @@
-clientId = "wantify-app"
-
-devBaseUrlString = "https://dev.apiv2.wantify.com/api/v1"
-prodBaseUrlString = "https://api.v2.wantify.com/api/v1"
-
-devTokenIsuer = "https://id.dev.apiv2.wantify.com/auth/realms/wantify"
-prodTokenIsuer = "https://id.v2.wantify.com/auth/realms/wantify"
-
-redirectUrlStr = "wantify://wantify"
